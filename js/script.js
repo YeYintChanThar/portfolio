9ecbb03e25fee8c94a69688af6abe9f002783801
Ye Yint Chan Thar <yeyintchanthar49@gmail.com>
2022-08-24 17:24:57 +0630

    first-commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(function(){
+    $('.chart').easyPieChart({
+        size:160,
+        barColor:"#f5b209",
+        scaleLength:0,
+        lineWidth:15,
+        trackColor:"#000080",
+        lineCap :"circle",
+        animate:2000,
+    })
+})
